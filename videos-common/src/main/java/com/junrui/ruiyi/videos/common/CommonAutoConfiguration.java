3d9cd89f0898bb1bbcf6dee62d2dbdff28def09c
tiancaijunrui <tiancaijunrui@gmail.com>
2023-05-29 22:42:42 +0800

    add common

--- videos-common/src/main/java/com/junrui/ruiyi/videos/common/CommonAutoConfiguration.java
+++ videos-common/src/main/java/com/junrui/ruiyi/videos/common/CommonAutoConfiguration.java
@@ -1,4 +1,17 @@
 package com.junrui.ruiyi.videos.common;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import net.bramp.ffmpeg.FFmpeg;
+
 public class CommonAutoConfiguration {
+
+    @Bean
+    public FFmpeg ffmpeg(@Value("${ffmpeg.path}") String ffmpegPath) throws IOException {
+        return new FFmpeg(ffmpegPath);
+    }
+
 }
---
+++ videos-common/src/main/java/com/junrui/ruiyi/videos/common/service/RvFfmpegService.java
@@ -0,0 +1,12 @@
+package com.junrui.ruiyi.videos.common.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ffmpeg 工具类
+ */
+@Service("rvFfmpegService")
+public class RvFfmpegService {
+    
+
+}
